83e67f91719068854d630c62da02b0f70c62f0f1
unknown <Visa-Valtteri Pimi@.(none)>
2011-09-08 18:31:14 +0300

    added 2x2 writer

--- sidplayer.asm
+++ sidplayer.asm
@@ -1,5 +1,10 @@
 .var music = LoadSid("turku_demoedit.sid")
 
+.var demopart_jmp = $000e
+.var pointer_lo = $000f
+.var pointer_hi = $0010
+
+
 .align $100
 
 framecounter: .byte 0
@@ -8,21 +13,29 @@ current_pattern_pointer: .byte 0
 
 .var num_patterns = 2
 
-demopart_jmp:
-	.byte 0
-pointer_lo:
-	.byte 0
-pointer_hi:
-	.byte 0
-
 pattern_orderlist_module:
-	.word writer, start
+			.word writer, writer, writer, writer
 
 pattern_orderlist_params:
-	.word writer1_params
+			.word writer1_params, writer1_params, writer1_params, writer1_params
 
 :BasicUpstart2(start)
 start:
+
+!loop:
+			.for(var i=0; i<4; i++) {
+				lda #$20
+				sta $0400+i*$100,x
+				lda #$02
+				sta $d800+i*$100,x
+			}
+			inx
+			bne !loop-
+
+			lda #$00
+			sta $d020
+			sta $d021
+
 			ldx #0
 			ldy #0
 
@@ -66,24 +79,222 @@ module_exit:
 
 // -- modules ---------------------------------------------
 
-// Simple 2x2 text writer for all the demo textscreens
-writer:
+textline_ypos: .byte 0, 0, 0
+textline_color: .byte 0, 0, 0
+text_animspeed: .byte 0
 
-	jmp module_exit
+cur_textline_xpos: .byte 0
+cur_textline_ypos: .byte 0
+cur_textline_len: .byte 0
+text_param_bytecounter: .byte 0
+new_screen_flag: .byte 0
 
-// writer params
-//
-// word text_data
+textline_offsets: .byte 0, 40*2, 40*4
+textdata_offset: .byte 0
+current_line: .byte 0
+
+color_offset: .byte 0
+
+.var screen = $0400
+
+// line entry
 // byte ypos (0-4)
 // byte color (c64 color)
+// word text_data
+// byte $FF text end
+//
+// byte $FF lines end
+// byte animspeed // update animation after how many frames? 
+
+// Simple 2x2 text writer for all the demo textscreens
+
+writer:
+			// set correct charset bank
+			lda #$1e
+			sta $d018
+
+			lda new_screen_flag
+			cmp #0 // are we starting a new writerscreen?
+			beq init_new_screen
+
+			jmp update_writer // just update if not new
+
+init_new_screen:
+
+			// --- init new writerscreen
+
+			lda #1
+			sta new_screen_flag
+
+			ldx current_pattern_pointer // get params for current pattern
+
+			lda pattern_orderlist_params,x
+			sta pointer_lo
+
+			lda pattern_orderlist_params+1,x
+			sta pointer_hi
+		
+writerloop:
+			lda pointer_lo
+			clc
+			adc text_param_bytecounter
+			sta pointer_lo // advance param bytepointer
+
+			lda #0
+			sta text_param_bytecounter
+
+			// param byte 0: text ypos
+			ldy #0
+			lda (pointer_lo),y
+			tax
+			
+			lda textline_offsets,x // get vertical screen offset for line ypos
+			sta textline_ypos,x    // store it here
+			sta cur_textline_ypos
+			stx current_line
+
+			inc text_param_bytecounter
+
+			// param byte 1: text color
+			ldy #1
+			lda (pointer_lo),y
+			sta textline_color,x
+			inc text_param_bytecounter
+		
+			// text writing loop
+
+			lda #0
+			sta cur_textline_len
+			sta cur_textline_xpos
+
+			// calculate line length	
+			// param byte 2: text start
+linecounter:
+			iny
+			inc text_param_bytecounter
+			inc cur_textline_len
+			lda (pointer_lo),y
+			cmp #255
+			bne linecounter
+
+			// calculate centered xpos
+
+			lda #20 // screenw / 2
+			clc
+			sbc cur_textline_len
+			sta cur_textline_xpos
+				
+			ldy #2
+			sty textdata_offset
+printloop:
+			ldy textdata_offset
+			lda (pointer_lo),y
+			tax // save char to x for lower char printing
+
+			pha
+
+			tya
+			clc
+			adc cur_textline_xpos
+			clc
+			adc cur_textline_ypos // calculate screen position
+			tay
+
+			pla
+
+			// print the new chars 2x2
+			sta screen+400,y
+			clc
+			adc #$40
+			sta screen+401,y
+
+			txa
+			ora #$80
+			sta screen+440,y
+
+			clc
+			adc #$40
+			sta screen+441,y
+
+			inc cur_textline_xpos
+
+			inc textdata_offset
+			ldy textdata_offset
+
+			lda (pointer_lo),y
+			cmp #255
+			bne printloop // more chars in string
+
+peeker:
+			
+			// peek if end of this screen
+			iny
+			lda (pointer_lo),y
+			cmp #255
+			beq update_writer // all lines rendered!
+			jmp writerloop	  // if not, render next line		
+			
+update_writer:
+
+			// color and fading here
+
+			// update line colors
+
+
+			ldy #0
+colorloop:
+			ldx #80
+			lda textline_offsets,y
+			sta color_offset
+colorloop0:
+			inc color_offset
+			txa
+			pha
+			
+			lda textline_color,y
+			ldx color_offset
+			sta $d800+399,x
+			pla
+			tax
+			dex
+			bne colorloop0
+			iny
+			cpy #3
+			bne colorloop
+			jmp module_exit
+
+// writer params
+// 
+// line entry
+// byte ypos (0-2)
+// byte color (c64 color)
+// word text_data
+// byte $FF text end
+//
+// byte $FF lines end
 // byte animspeed // update animation after how many frames? 
 
 //---------------------------------------------------------
+
 writer1_params:
-	.text "this is a test"
-	.byte 2
-	.byte 15
-	.byte 0  
+			.byte 0  // ypos
+			.byte LIGHT_GREEN // color
+			.text "hei rakkaat ystavat"  // max 20 chars
+			.byte $FF
+
+			.byte 1  // ypos
+			.byte LIGHT_BLUE // color
+			.text "ja tervetuloa"
+			.byte $FF
+
+			.byte 2  // ypos
+			.byte LIGHT_GRAY // color
+			.text "shown pariin."
+			.byte $FF
+
+			.byte $FF // end
+
+
 
 //---------------------------------------------------------
 irq1:  	
@@ -97,7 +308,7 @@ pattern_logic:
 
 // frame-based timing, change to goat-timing etc.
 			lda framecounter 
-			cmp #64
+			cmp #192
 			beq nextpattern
 			jmp pattern_logic_done
 nextpattern:
@@ -117,9 +328,9 @@ pattern_logic_done:
 //---------------------------------------------------------
 .pc=music.location "Music"
 .fill music.size, music.getData(i)
-// .pc = $0c00	"ScreenRam" 			.fill picture.getScreenRamSize(), picture.getScreenRam(i)
-// .pc = $4c00	"ColorRam:" colorRam: 	.fill picture.getColorRamSize(), picture.getColorRam(i)
-// .pc = $2000	"Bitmap"				.fill picture.getBitmapSize(), picture.getBitmap(i)
+
+.pc = $3800
+.import binary "font.bin"
 
 .print ""
 .print "SID Data"
